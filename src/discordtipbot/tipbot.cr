e448ea4b793b5427068f4869250fb0d3853f816c
Jonathan B <greenbigfrog@gmail.com>
2017-02-13 17:18:00 +0100

    fix @db type issue

--- src/discordtipbot/tipbot.cr
+++ src/discordtipbot/tipbot.cr
@@ -1,4 +1,5 @@
 class Tipbot
+  @db : DB::Database
   def initialize(@config : Config, @log : Logger)
     @log.debug("Starting bot: #{@config.coinname_full}")
     @bot = Discord::Client.new(token: @config.discord_token, client_id: @config.discord_client_id)
