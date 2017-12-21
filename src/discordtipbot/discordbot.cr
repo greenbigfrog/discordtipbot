34a890ba472b9183960755b77728d3b6a791af8c
greenbigfrog <greenbigfrog@gmail.com>
2017-12-21 20:09:58 +0000

    fix typo

--- src/discordtipbot/discordbot.cr
+++ src/discordtipbot/discordbot.cr
@@ -1,6 +1,6 @@
 class DiscordBot
   def initialize(@config : Config, @log : Logger)
-    @log.debug("#{@config.coinname_short}: tarting bot: #{@config.coinname_full}")
+    @log.debug("#{@config.coinname_short}: starting bot: #{@config.coinname_full}")
     @bot = Discord::Client.new(token: @config.discord_token, client_id: @config.discord_client_id)
     @tip = TipBot.new(@config, @log)
   end
