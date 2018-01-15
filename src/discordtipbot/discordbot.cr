074f847dc2b9cc82c6d6650c581668aa9e816b12
IncognitoJam <4038174+IncognitoJam@users.noreply.github.com>
2018-01-15 18:00:48 +0000

    Implemented amount-per-user for ~rain, untested\nAlso made total amount bold for ~rain

--- src/discordtipbot/discordbot.cr
+++ src/discordtipbot/discordbot.cr
@@ -341,7 +341,7 @@ class DiscordBot
         targets.each { |x| string = string + ", #{@cache.resolve_user(x).username}" }
       end
       string = string.lchop(", ")
-      reply(msg, "#{msg.author.username} soaked a total of #{amount} #{@config.coinname_short} (#{each_amount} #{@config.coinname_short} each) onto #{string}")
+      reply(msg, "#{msg.author.username} soaked a total of **#{amount} #{@config.coinname_short}** (#{each_amount} #{@config.coinname_short} each) onto #{string}")
     end
   end
 
@@ -377,6 +377,7 @@ class DiscordBot
       reply(msg, "**ERROR**: Please try again later")
     when true
       string = ""
+      each_amount = amount / users.size
 
       if @tip.get_config(guild_id(msg), "mention")
         authors.each { |x| string = string + ", <@#{x}>" }
@@ -384,7 +385,7 @@ class DiscordBot
         authors.each { |x| string = string + ", #{@cache.resolve_user(x).username}" }
       end
       string = string.lchop(", ")
-      reply(msg, "#{msg.author.username} rained a total of **#{amount} #{@config.coinname_short}** onto #{string}")
+      reply(msg, "#{msg.author.username} rained a total of **#{amount} #{@config.coinname_short}** (#{each_amount} #{@config.coinname_short} each) onto #{string}")
     end
   end
 
