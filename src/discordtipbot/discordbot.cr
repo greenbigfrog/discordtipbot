16dad0e5aad3114dd385ef1dcc400187712de421
IncognitoJam <4038174+IncognitoJam@users.noreply.github.com>
2018-01-15 17:58:23 +0000

    Implemented amount-per-user for ~soak, untested

--- src/discordtipbot/discordbot.cr
+++ src/discordtipbot/discordbot.cr
@@ -333,6 +333,7 @@ class DiscordBot
       reply(msg, "**ERROR**: Please try again later")
     when true
       string = ""
+      each_amount = amount / targets.size
 
       if @tip.get_config(guild_id(msg), "mention")
         targets.each { |x| string = string + ", <@#{x}>" }
@@ -340,7 +341,7 @@ class DiscordBot
         targets.each { |x| string = string + ", #{@cache.resolve_user(x).username}" }
       end
       string = string.lchop(", ")
-      reply(msg, "#{msg.author.username} soaked a total of #{amount} #{@config.coinname_short} onto #{string}")
+      reply(msg, "#{msg.author.username} soaked a total of #{amount} #{@config.coinname_short} (#{each_amount} #{@config.coinname_short} each) onto #{string}")
     end
   end
 
