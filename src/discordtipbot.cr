8e622a82aeae30a095490650fa3dc68e4e8172dd
Zac Nowicki <zachnowicki@gmail.com>
2018-01-18 14:14:36 -0500

    Don't run the bot in a test environment
    Set TIPBOT_ENV for travis

--- src/discordtipbot.cr
+++ src/discordtipbot.cr
@@ -4,29 +4,31 @@ require "logger"
 require "pg"
 require "discordcr"
 
-puts "No Config File specified! Exiting!" if ARGV.size == 0
-exit if ARGV.size == 0
+unless ENV["TIPBOT_ENV"]? == "test"
+  puts "No Config File specified! Exiting!" if ARGV.size == 0
+  exit if ARGV.size == 0
 
-log = Logger.new(STDOUT)
+  log = Logger.new(STDOUT)
 
-# Set your logger level here
-log.level = Logger::DEBUG
+  # Set your logger level here
+  log.level = Logger::DEBUG
 
-log.debug("Tipbot network getting started")
+  log.debug("Tipbot network getting started")
 
-log.debug("Attempting to read config from \"#{ARGV[0]}\"")
-config = File.open(ARGV[0], "r") do |file|
-  Array(Config).from_json(file)
-end
-log.info("read config from \"#{ARGV[0]}\"")
-
-log.debug("starting forking")
-bots = config.each do |x|
-  spawn do
-    Controller.new(x, log)
+  log.debug("Attempting to read config from \"#{ARGV[0]}\"")
+  config = File.open(ARGV[0], "r") do |file|
+    Array(Config).from_json(file)
   end
-end
-log.debug("finished forking")
+  log.info("read config from \"#{ARGV[0]}\"")
 
-log.info("All bots should be running now")
-sleep
+  log.debug("starting forking")
+  bots = config.each do |x|
+    spawn do
+      Controller.new(x, log)
+    end
+  end
+  log.debug("finished forking")
+
+  log.info("All bots should be running now")
+  sleep
+end
