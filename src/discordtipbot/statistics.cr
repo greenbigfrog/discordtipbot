8fc2570685685f40e6c51d863c0df09b4e2477b8
greenbigfrog <greenbigfrog@gmail.com>
2018-02-10 22:27:21 +0100

    fixup! Add in detail statistics

--- src/discordtipbot/statistics.cr
+++ src/discordtipbot/statistics.cr
@@ -15,11 +15,11 @@ class Statistics
   end
 
   private def update_statistics
-    @transactions = @db.query_one("SELECT SUM(1) FROM transactions", as: Int64?)
-    @total = @db.query_one("SELECT SUM(amount) FROM transactions", as: BigDecimal?)
-    @tips = @db.query_one("SELECT SUM(amount) FROM transactions WHERE memo='tip'", as: BigDecimal?)
-    @soaks = @db.query_one("SELECT SUM(amount) FROM transactions WHERE memo='soak'", as: BigDecimal?)
-    @rains = @db.query_one("SELECT SUM(amount) FROM transactions WHERE memo='rain'", as: BigDecimal?)
+    @transactions = @db.query_one("SELECT SUM(1) FROM transactions", as: Int64)
+    @total = @db.query_one("SELECT SUM(amount) FROM transactions", as: BigDecimal)
+    @tips = @db.query_one("SELECT SUM(amount) FROM transactions WHERE memo='tip'", as: BigDecimal)
+    @soaks = @db.query_one("SELECT SUM(amount) FROM transactions WHERE memo='soak'", as: BigDecimal)
+    @rains = @db.query_one("SELECT SUM(amount) FROM transactions WHERE memo='rain'", as: BigDecimal)
     @last = Time.utc_now
   end
 
