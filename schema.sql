0243e4387fae03a94f21534b502cc23bf41273c8
greenbigfrog <greenbigfrog@gmail.com>
2018-02-11 13:19:32 +0100

    Provide SQL to create statistics materialized view

--- schema.sql
+++ schema.sql
@@ -39,5 +39,13 @@ CREATE TABLE coin_transactions (
        status text NOT NULL,
 
        created_time timestamptz NOT NULL DEFAULT now()
-)
+);
 
+CREATE MATERIALIZED VIEW statistics AS (
+       SELECT
+              (SELECT SUM(1) FROM transactions) AS transaction_count,
+              (SELECT SUM(amount) FROM transactions) AS transaction_sum,
+              (SELECT SUM(amount) FROM transactions WHERE memo='tip') AS tip_sum,
+              (SELECT SUM(amount) FROM transactions WHERE memo='soak') AS soak_sum,
+              (SELECT SUM(amount) FROM transactions WHERE memo='rain') as rain_sum
+);
