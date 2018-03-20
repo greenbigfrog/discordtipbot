e64886dbbd00284cf3e7e3c9487f8f713342aed9
greenbigfrog <greenbigfrog@gmail.com>
2018-03-20 16:09:47 +0100

    Improve SQL for transactions

--- schema.sql
+++ schema.sql
@@ -35,7 +35,7 @@ CREATE TABLE config (
 );
 
 CREATE TABLE coin_transactions (
-       txhash text PRIMARY KEY,
+       txhash text PRIMARY KEY UNIQUE,
        status text NOT NULL,
 
        created_time timestamptz NOT NULL DEFAULT now()
