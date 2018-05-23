ea91bd14f342f37c6fd3776a61775201dcd8516a
Anthony <chenanthony365@gmail.com>
2018-05-23 19:14:50 -0400

    Update discordbot.cr
    oops spelling

--- src/discordtipbot/discordbot.cr
+++ src/discordtipbot/discordbot.cr
@@ -5,7 +5,7 @@ class DiscordBot
 
   USER_REGEX = /<@!?(?<id>\d+)>/
   START_TIME = Time.now
-  TERMS      = "In no event shall this bot, it's dev, or other contributors be responsible for any loss, theft or misdirection of funds. Remember to double check all deposits and withdrawlas!"
+  TERMS      = "In no event shall this bot, it's dev, or other contributors be responsible for any loss, theft or misdirection of funds. Remember to double check all deposits and withdrawals!"
   ZWS        = "​" # There is a zero width space stored here
 
   @unavailable_guilds = Set(UInt64).new
