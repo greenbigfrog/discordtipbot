7bbff9109219e71fed78d320a2ca364101cc6b8e
IncognitoJam <cameronjclough@gmail.com>
2018-08-10 16:50:03 +0100

    Remove footer from embed

--- src/discordtipbot/discordbot.cr
+++ src/discordtipbot/discordbot.cr
@@ -478,7 +478,6 @@ class DiscordBot
     begin
       address = @tip.get_address(msg.author.id.to_u64)
       embed = Discord::Embed.new(
-        footer: Discord::EmbedFooter.new("I love you! ❤"),
         image: Discord::EmbedImage.new("https://chart.googleapis.com/chart?cht=qr&chs=300x300&chld=L%7C1&chl=#{URI.escape(@config.uri_scheme)}:#{address}")
       )
       @bot.create_message(@cache.resolve_dm_channel(msg.author.id.to_u64), "Your deposit address is: **#{address}**\nPlease keep in mind, that this address is for **one time use only**. After every deposit your address will reset! Don't use this address to receive from faucets, pools, etc.\nDeposits take **#{@config.confirmations} confirmations** to get credited!\n*#{TERMS}*")# embed)
