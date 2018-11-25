4fa85306951bbc1289086a05345726b2625b9347
Jonathan B <greenbigfrog@gmail.com>
2018-11-25 13:36:14 +0100

    Print exception with backtrace

--- src/discordtipbot/middleware/errorcatcher.cr
+++ src/discordtipbot/middleware/errorcatcher.cr
@@ -4,5 +4,6 @@ class ErrorCatcher
   rescue ex
     context[Discord::Client].create_message(payload.channel_id, "There was an unexpected error. This has been reported and should be resolved soon") if payload.is_a?(Discord::Message)
     Raven.capture(Exception.new("Exception while handling message #{payload}", ex))
+    puts ex.inspect_with_backtrace
   end
 end
