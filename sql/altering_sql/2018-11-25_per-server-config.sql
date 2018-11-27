9e09c2a6133777bddd9be247bac0642d625ae9f3
Jonathan B <greenbigfrog@gmail.com>
2018-11-27 15:24:15 +0100

    Allow string as prefix

--- sql/altering_sql/2018-11-25_per-server-config.sql
+++ sql/altering_sql/2018-11-25_per-server-config.sql
@@ -3,6 +3,6 @@ ALTER TABLE config ADD min_soak_total numeric(64, 8);
 ALTER TABLE config ADD min_rain numeric(64, 8);
 ALTER TABLE config ADD min_rain_total numeric(64, 8);
 ALTER TABLE config ADD min_tip numeric(64, 8);
-ALTER TABLE config ADD prefix char;
+ALTER TABLE config ADD prefix text;
 ALTER TABLE config ADD premium boolean DEFAULT false;
 ALTER TABLE config ADD premium_till timestamp;
