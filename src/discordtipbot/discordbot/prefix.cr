784ae74d7387143dc2a1c8b36eac2153194f7201
Jonathan B <greenbigfrog@gmail.com>
2018-11-28 16:39:41 +0100

    Allow empty custom prefix

--- src/discordtipbot/discordbot/prefix.cr
+++ src/discordtipbot/discordbot/prefix.cr
@@ -9,12 +9,16 @@ class Prefix
     # cmd[0] = new_prefix
     cmd = ctx[Command].command
 
-    return client.create_message(msg.channel_id, "Usage: `.prefix \"[new_prefix]\"`") if cmd.empty?
+    return client.create_message(msg.channel_id, "Usage: `.prefix \"[new_prefix/clear]\"`\nCurrent prefix is `#{ctx[ConfigMiddleware].get_prefix(msg)}`") if cmd.empty?
 
     guild_id = cache.resolve_channel(msg.channel_id).guild_id.try &.to_u64
     return if guild_id.nil?
 
     string = cmd.join(' ').strip('"')
+
+    if string.starts_with?("clear")
+      return client.create_message(msg.channel_id, "Successfully cleared prefix. Only prefix is mentioning now") if @tip.clear_config("prefix", guild_id)
+    end
 
     return client.create_message(msg.channel_id, "Successfully set the prefix to **`#{string + ZWS}`**") if @tip.update_config("prefix", string, guild_id)
     client.create_message(msg.channel_id, "**ERROR:** Please try again or get support at <http://tipbot.gbf.re>")
