ef214cd6e96b7b743a7e7aa3a8ceef78d3f5bf7c
Jonathan B <greenbigfrog@gmail.com>
2018-11-29 11:52:46 +0100

    Add note on how users can get premium for voting

--- src/discordtipbot/middleware/premium_only.cr
+++ src/discordtipbot/middleware/premium_only.cr
@@ -3,7 +3,17 @@ class PremiumOnly
     if ctx[ConfigMiddleware].get_config(msg, "premium")
       yield
     else
-      return ctx[Discord::Client].create_message(msg.channel_id, "This is a premium only feature. For more info please visit <http://tipbot.gbf.re>")
+      cache = ctx[Discord::Client].cache.not_nil!
+      string = String::Builder.build do |io|
+        io.puts "This is a premium only feature. For more info please visit <http://tipbot.gbf.re>"
+        io.puts
+        io << "*Get **15 Minutes premium by voting** at https://discordbots.org/bot/"
+        io << cache.try &.resolve_current_user.id
+        io << "/vote?server="
+        io << cache.resolve_channel(msg.channel_id).guild_id
+        io << '*'
+      end
+      return ctx[Discord::Client].create_message(msg.channel_id, string)
     end
   end
 end
