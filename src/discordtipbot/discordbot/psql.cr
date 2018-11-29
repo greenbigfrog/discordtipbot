cece2b52d812a1dc5fa470b84616564d3dd049e6
Jonathan B <greenbigfrog@gmail.com>
2018-11-29 14:03:50 +0100

    Add psql command

---
+++ src/discordtipbot/discordbot/psql.cr
@@ -0,0 +1,23 @@
+class PSQL
+  def initialize(@db : DB::Database, @config : Config)
+  end
+
+  def call(msg, ctx)
+    client = ctx[Discord::Client]
+
+    cmd = ctx[Command].command.join(' ')
+    return client.create_message(msg.channel_id, "No SQL query specified") if cmd.empty?
+    return client.create_message(msg.channel_id, "DROPing isn't allowed") if cmd.match /drop/i
+
+    args = ["-d#{@config.coinname_full.downcase}"]
+    args.push "-c#{cmd}"
+    str = Process.new("psql", args: args, input: Process::Redirect::Inherit,
+      output: Process::Redirect::Pipe, error: Process::Redirect::Inherit).output.gets_to_end
+
+    return client.create_message(msg.channel_id,
+      "**Output too big. Sending first 1.5k characes.**\n```#{str[0..1500]}```") if str.size > 2000
+
+    client.create_message(msg.channel_id, "```#{str}```")
+    yield
+  end
+end
