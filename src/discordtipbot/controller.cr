4cc116a02a304d82fbf1a6a0bbf18329808bb2fe
Jonathan B <greenbigfrog@gmail.com>
2018-12-09 20:28:07 +0100

    Get rid of unnecessary Controller

--- src/discordtipbot/controller.cr
+++
@@ -1,7 +0,0 @@
-class Controller
-  def initialize(@config : Config, @log : Logger)
-    @log.info("#{@config.coinname_short}: Fork starting #{@config.coinname_full} bot")
-    @bot = DiscordBot.new(@config, @log)
-    @bot.run
-  end
-end
