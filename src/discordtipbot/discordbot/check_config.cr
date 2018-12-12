d0e5dc88ebd8fcb7a2a7ea862564abf46fd33f6d
Jonathan B <greenbigfrog@gmail.com>
2018-12-12 16:49:57 +0100

    Improve DM dependend fields inserting in checkconfig

--- src/discordtipbot/discordbot/check_config.cr
+++ src/discordtipbot/discordbot/check_config.cr
@@ -1,18 +1,22 @@
 class CheckConfig
+  include DiscordMiddleware::CachedRoutes
+
   def call(msg, ctx)
-    fields = [
-      Discord::EmbedField.new("Min Soak", ctx[ConfigMiddleware].get_decimal_config(msg, "min_soak").to_s, true),
-      Discord::EmbedField.new("Min Soak Total", ctx[ConfigMiddleware].get_decimal_config(msg, "min_soak_total").to_s, true),
-      Discord::EmbedField.new("Min Rain", ctx[ConfigMiddleware].get_decimal_config(msg, "min_rain").to_s, true),
-      Discord::EmbedField.new("Min Rain Total", ctx[ConfigMiddleware].get_decimal_config(msg, "min_rain_total").to_s, true),
-      Discord::EmbedField.new("Min Tip", ctx[ConfigMiddleware].get_decimal_config(msg, "min_tip").to_s, true),
-    ]
-    unless ctx[Discord::Client].cache.try &.resolve_channel(msg.channel_id).type == Discord::ChannelType::DM
-      fields.insert(0, Discord::EmbedField.new("Soak", string(ctx[ConfigMiddleware].get_config(msg, "soak")), true))
-      fields.insert(3, Discord::EmbedField.new("Rain", string(ctx[ConfigMiddleware].get_config(msg, "rain")), true))
-      fields.insert(6, Discord::EmbedField.new("Mention", string(ctx[ConfigMiddleware].get_config(msg, "mention")), true))
-    end
-    ctx[Discord::Client].create_message(msg.channel_id,
+    client = ctx[Discord::Client]
+    channel = get_channel(client, msg.channel_id)
+    dm = channel.type.dm?
+
+    fields = Array(Discord::EmbedField).new(8)
+    fields << Discord::EmbedField.new("Soak", string(ctx[ConfigMiddleware].get_config(msg, "soak")), true) unless dm
+    fields << Discord::EmbedField.new("Min Soak", ctx[ConfigMiddleware].get_decimal_config(msg, "min_soak").to_s, true)
+    fields << Discord::EmbedField.new("Min Soak Total", ctx[ConfigMiddleware].get_decimal_config(msg, "min_soak_total").to_s, true)
+    fields << Discord::EmbedField.new("Rain", string(ctx[ConfigMiddleware].get_config(msg, "rain")), true) unless dm
+    fields << Discord::EmbedField.new("Min Rain", ctx[ConfigMiddleware].get_decimal_config(msg, "min_rain").to_s, true)
+    fields << Discord::EmbedField.new("Min Rain Total", ctx[ConfigMiddleware].get_decimal_config(msg, "min_rain_total").to_s, true)
+    fields << Discord::EmbedField.new("Mention", string(ctx[ConfigMiddleware].get_config(msg, "mention")), true) unless dm
+    fields << Discord::EmbedField.new("Min Tip", ctx[ConfigMiddleware].get_decimal_config(msg, "min_tip").to_s, true)
+
+    client.create_message(msg.channel_id,
       ZWS,
       Discord::Embed.new(
         description: "This commands represents the config of the current server. Edit it with `config`",
