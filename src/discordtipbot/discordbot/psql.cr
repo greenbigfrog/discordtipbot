64a42c4e484121e907fcb7b73ac22d8671f4998f
Jonathan B <greenbigfrog@gmail.com>
2018-12-13 18:14:04 +0100

    Disable PSQL command
    Doesn't work as easily anymore, since docker

--- src/discordtipbot/discordbot/psql.cr
+++ src/discordtipbot/discordbot/psql.cr
@@ -9,15 +9,16 @@ class PSQL
     return client.create_message(msg.channel_id, "No SQL query specified") if cmd.empty?
     return client.create_message(msg.channel_id, "DROPing isn't allowed") if cmd.match /drop/i
 
-    args = ["-d#{@config.coinname_full.downcase}"]
-    args.push "-c#{cmd}"
-    str = Process.new("psql", args: args, input: Process::Redirect::Inherit,
-      output: Process::Redirect::Pipe, error: Process::Redirect::Inherit).output.gets_to_end
+    # args = ["-d#{@config.coinname_full.downcase}"]
+    # args.push "-c#{cmd}"
+    # str = Process.new("psql", args: args, input: Process::Redirect::Inherit,
+    #   output: Process::Redirect::Pipe, error: Process::Redirect::Inherit).output.gets_to_end
 
-    return client.create_message(msg.channel_id,
-      "**Output too big. Sending first 1.5k characes.**\n```#{str[0..1500]}```") if str.size > 2000
+    # return client.create_message(msg.channel_id,
+    #   "**Output too big. Sending first 1.5k characes.**\n```#{str[0..1500]}```") if str.size > 2000
 
-    client.create_message(msg.channel_id, "```#{str}```")
+    # client.create_message(msg.channel_id, "```#{str}```")
+    client.create_message(msg.channel_id, "This command has been disabled for now")
     yield
   end
 end
