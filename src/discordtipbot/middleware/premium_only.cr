9b183dd13d4d895fbd702ab765b351938b0676c7
Jonathan B <greenbigfrog@gmail.com>
2018-12-13 16:54:16 +0100

    Improve PremiumOnly response

--- src/discordtipbot/middleware/premium_only.cr
+++ src/discordtipbot/middleware/premium_only.cr
@@ -5,17 +5,11 @@ class PremiumOnly
     if ctx[ConfigMiddleware].get_config(msg, "premium")
       yield
     else
-      client = ctx[Discord::Client]
-      string = String::Builder.build do |io|
-        io.puts "This is a premium only feature. For more info please visit <http://tipbot.gbf.re>"
-        io.puts
-        io << "*Get **15 Minutes premium by voting** at https://discordbots.org/bot/"
-        io << client.cache.not_nil!.resolve_current_user.id
-        io << "/vote?server="
-        io << get_channel(client, msg.channel_id).guild_id
-        io << '*'
-      end
-      return ctx[Discord::Client].create_message(msg.channel_id, string)
+      return ctx[Discord::Client].create_message(msg.channel_id, <<-STRING)
+      This is a premium only feature. For more info please visit <http://tipbot.gbf.re>
+
+      *Get Free Premium by voting. Check `#{ctx[ConfigMiddleware].get_prefix(msg)}vote` for more info*
+      STRING
     end
   end
 end
