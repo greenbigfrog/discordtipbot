f8cb87a58a0219c505a6d1d09eacafcf18ed458a
Jonathan B <greenbigfrog@gmail.com>
2018-12-14 18:32:34 +0100

    Truncate and retry sending Sentry Exception if too long etc

--- src/discordtipbot/middleware/errorcatcher.cr
+++ src/discordtipbot/middleware/errorcatcher.cr
@@ -3,7 +3,13 @@ class ErrorCatcher
     yield
   rescue ex
     context[Discord::Client].create_message(payload.channel_id, "There was an unexpected error. This has been reported and should be resolved soon") if payload.is_a?(Discord::Message)
-    Raven.capture(Exception.new("Exception while handling message #{payload}", ex))
     puts ex.inspect_with_backtrace
+
+    begin
+      Raven.capture(Exception.new("Exception while handling message #{payload}", ex))
+    rescue
+      # Truncate and retry if previous sending failed
+      Raven.capture(Exception.new("Exception while handling message #{payload.to_s[0..2000]}"))
+    end
   end
 end
