4a74f310a88cec88a4c4d80a87bc7396e5d884e2
Jonathan B <greenbigfrog@gmail.com>
2018-12-14 18:44:52 +0100

    Truncate all Sentry Exceptions

--- src/discordtipbot/middleware/errorcatcher.cr
+++ src/discordtipbot/middleware/errorcatcher.cr
@@ -5,11 +5,7 @@ class ErrorCatcher
     context[Discord::Client].create_message(payload.channel_id, "There was an unexpected error. This has been reported and should be resolved soon") if payload.is_a?(Discord::Message)
     puts ex.inspect_with_backtrace
 
-    begin
-      Raven.capture(Exception.new("Exception while handling message #{payload}", ex))
-    rescue
-      # Truncate and retry if previous sending failed
-      Raven.capture(Exception.new("Exception while handling message #{payload.to_s[0..2000]}"))
-    end
+    # Truncate all payloads for now
+    Raven.capture(Exception.new("Exception while handling message #{payload.to_s[0..2000]}"))
   end
 end
