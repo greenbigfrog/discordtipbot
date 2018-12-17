d959f682f80e9c01231ff2174592f84955d43f02
Jonathan B <greenbigfrog@gmail.com>
2018-12-17 11:04:03 +0100

    Do not send error message if it was DB related

--- src/discordtipbot/middleware/errorcatcher.cr
+++ src/discordtipbot/middleware/errorcatcher.cr
@@ -2,7 +2,7 @@ class ErrorCatcher
   def call(payload, context)
     yield
   rescue ex
-    context[Discord::Client].create_message(payload.channel_id, "There was an unexpected error. This has been reported and should be resolved soon") if payload.is_a?(Discord::Message)
+    context[Discord::Client].create_message(payload.channel_id, "There was an unexpected error. This has been reported and should be resolved soon") unless ex == DB::PoolRetryAttemptsExceeded if payload.is_a?(Discord::Message)
     puts ex.inspect_with_backtrace
 
     # Truncate all payloads for now
