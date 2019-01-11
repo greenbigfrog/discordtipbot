7c3fc7d42d3a3f2fe50477ee9a224c06a238fa95
Jonathan B <greenbigfrog@gmail.com>
2019-01-11 19:57:39 +0100

    Do not send Error message to discord if due to db unavailabilty

--- src/discordtipbot/middleware/errorcatcher.cr
+++ src/discordtipbot/middleware/errorcatcher.cr
@@ -2,7 +2,11 @@ class ErrorCatcher
   def call(payload, context)
     yield
   rescue ex
-    context[Discord::Client].create_message(payload.channel_id, "There was an unexpected error. This has been reported and should be resolved soon") unless ex == DB::PoolRetryAttemptsExceeded if payload.is_a?(Discord::Message)
+    if payload.is_a?(Discord::Message)
+      unless ex.is_a?(DB::PoolRetryAttemptsExceeded)
+        context[Discord::Client].create_message(payload.channel_id, "There was an unexpected error. This has been reported and should be resolved soon")
+      end
+    end
     puts ex.inspect_with_backtrace
 
     # Truncate all payloads for now
