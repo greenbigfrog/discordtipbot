1f574e2b4ee62f2e654aa4ec6e7129610cae3b7c
Jonathan B <greenbigfrog@gmail.com>
2019-01-13 21:23:24 +0100

    Move DATA and LOG to common constants

--- src/discord-launcher.cr
+++ src/discord-launcher.cr
@@ -1,8 +1,4 @@
 require "./discord/discordtipbot"
 require "./common/constants"
 
-# TODO improve this
-LOG  = Logger.new(STDOUT)
-DATA = PG.connect(ENV["POSTGRES"]? || POSTGRES)
-
 DiscordTipBot.run
--- src/twitch-launcher.cr
+++ src/twitch-launcher.cr
@@ -2,8 +2,4 @@ require "./twitch/twitchtipbot"
 
 require "./common/constants"
 
-# TODO improve this
-LOG  = Logger.new(STDOUT)
-DATA = PG.connect(ENV["POSTGRES"]? || POSTGRES)
-
 TwitchTipBot.run
