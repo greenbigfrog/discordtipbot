7c9507336b67c9687447bf70bf22a862b0036a1f
Jonathan B <greenbigfrog@gmail.com>
2019-07-17 13:39:07 +0200

    Initial docker-compose setup

--- scripts/postgres-init.sh
+++ scripts/postgres-init.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c 'CREATE USER docker'
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c 'CREATE DATABASE dogecoin'
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c 'GRANT ALL PRIVILEGES ON DATABASE dogecoin TO docker'
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c 'CREATE DATABASE tipbot'
+
 if [ -f "/dump.sql" ]
 then
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname dogecoin -f /dump.sql
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname tipbot -f /dump.sql
 else
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname dogecoin -f /schema.sql
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname tipbot -f /schema.sql
 fi
-
-# Copy and modify above to initialize more databases for the various coins
