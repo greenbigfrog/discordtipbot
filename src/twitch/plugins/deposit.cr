b16cd8ea8f719187e0acef2496d278289662b2b8
Jonathan B <greenbigfrog@gmail.com>
2019-08-13 15:12:03 +0200

    Get rid of useless comment and useless ID checking

--- src/twitch/plugins/deposit.cr
+++ src/twitch/plugins/deposit.cr
@@ -5,14 +5,6 @@ module ChatBot::Plugins::Deposit
     bot.on(PRIVWHISP, message: /^#{coin.prefix}deposit/, doc: {"deposit", "Tells the user their address to deposit coins to the bot to"}) do |msg|
       name = msg.display_name || ChatBot.extract_nick(msg.source)
 
-      raise NO_USER_ID unless id = msg.user_id
-
-      # address = Data::DepositAddress.read_or_create(@coin, Data::Account.read(:twitch, id))
-      # if address.is_a?(Data::Error)
-      #   return client.create_message(msg.channel_id, "Something went wrong. Please try again later, or request help at #{SUPPORT}")
-      # end
-
-      # string = "Your deposit address is #{address}"
       string = "Please visit https://tipbot.info/deposit to retrieve your deposit address"
       bot.reply(msg, ChatBot.mention(name, string))
     end
